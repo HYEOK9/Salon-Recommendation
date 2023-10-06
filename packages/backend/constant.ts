3c7db7387939bb182a2cccba66b600bd10d21f2a
HYEOK9 <leejaehyuck9@gmail.com>
2023-10-06 19:13:27 +0900

    [FEAT] add crawling

---
+++ packages/backend/constant.ts
@@ -0,0 +1,38 @@
+import chrome from "selenium-webdriver/chrome";
+
+const TEST_MODE = process.env.CRAWLING_NODE_ENV === "development";
+
+const chromeOptions = new chrome.Options().addArguments(
+  "--disable-gpu",
+  "window-size=1920x1080",
+  "lang=ko_KR"
+);
+
+if (!TEST_MODE) chromeOptions.addArguments("--headless=new");
+
+// 거리순 정렬하려면 mobile view로 열어야함
+const placeResultUrl =
+  "https://m.map.naver.com/search2/search.naver?query=%EB%AF%B8%EC%9A%A9%EC%8B%A4&siteSort=1&sm=clk";
+
+const script = {
+  scrollBottom: "window.scrollTo(0, document.body.scrollHeight)",
+  getScrollY: "return document.body.scrollHeight",
+};
+
+const MAX_SALON = Number(process.env.MAX_SALON);
+const MAX_IMAGE_LENGTH_PER_SALON = Number(
+  process.env.MAX_IMAGE_LENGTH_PER_SALON
+);
+const SCROLL_TIME = 500;
+const SCROLL_TIMEOUT = Number(process.env.SCROLL_TIMEOUT);
+
+export {
+  TEST_MODE,
+  chromeOptions,
+  placeResultUrl,
+  script,
+  MAX_SALON,
+  MAX_IMAGE_LENGTH_PER_SALON,
+  SCROLL_TIME,
+  SCROLL_TIMEOUT,
+};
---
+++ packages/backend/index.ts
@@ -0,0 +1,146 @@
+import { Builder, By, until } from "selenium-webdriver";
+import {
+  TEST_MODE,
+  chromeOptions,
+  placeResultUrl,
+  script,
+  MAX_SALON,
+  MAX_IMAGE_LENGTH_PER_SALON,
+  SCROLL_TIME,
+  SCROLL_TIMEOUT,
+} from "./constant";
+
+const run = async () => {
+  let driver = await new Builder()
+    .forBrowser("chrome")
+    .setChromeOptions(chromeOptions)
+    .build();
+
+  let totalImageList: string[] = [];
+
+  try {
+    // 현재 위치에서 미용실을 검색했을 때 검색 결과
+    await driver.get(placeResultUrl);
+
+    await driver.findElement(By.id("_myLocation")).click();
+
+    let currentUrl = await driver.getCurrentUrl();
+
+    console.log("\n위치 가져오는 중...");
+
+    while (currentUrl === (await driver.getCurrentUrl())) {}
+
+    // 현 위치
+    let locTextElement = await driver.findElement(By.css("._resultQuery p"));
+
+    await driver.wait(until.elementIsVisible(locTextElement)).catch(() => {});
+
+    console.log(`${await locTextElement.getText()}\n`);
+
+    // 미용실 이름들
+    let resultElements = await driver.findElements(
+      By.className("_item _lazyImgContainer")
+    );
+
+    // 최댓값으로 설정한 만큼의 리뷰들만 가져옴
+    resultElements = resultElements.slice(0, MAX_SALON);
+
+    for (const salon of resultElements) {
+      // 미용실 이름
+      const placeName = await salon.getAttribute("data-title");
+      // 미용실 place id
+      const sid = await salon.getAttribute("data-sid");
+      // 해당 미용실의 방문자 review page로 이동
+      await driver
+        .navigate()
+        .to(
+          `https://m.place.naver.com/hairshop/${sid}/photo?filterType=방문자`
+        );
+
+      // 방문자 포토리뷰 담고있는 div
+      let reviewContainer = await driver.findElements(By.className("Nd2nM"));
+
+      // 리뷰 없으면 뒤로가서 다음 미용실 탐색
+      if (!reviewContainer.length) {
+        console.log(`${placeName}: 0개`);
+        await driver.navigate().back();
+        continue;
+      }
+
+      await driver.wait(until.elementIsVisible(reviewContainer[0]));
+
+      // 현재 보고있는 미용실의 리뷰이미지들 담을 배열
+      let imageList: string[] = [];
+
+      // 리뷰 페이지 body height
+      let prevHeight: number = await driver.executeScript(script.getScrollY);
+
+      while (1) {
+        // 페이지에 보이는 모든 리뷰 이미지 요소
+        let reviewImages = await driver.findElements(By.className("wzrbN"));
+
+        for (const reviewImage of reviewImages) {
+          // 이미 넣은 dataGridGroupKey를 제외하고 리뷰 이미지 배열에 추가\
+
+          let imageElement = await reviewImage.findElement(By.css("a > img"));
+
+          let visitorNumber = await imageElement.getAttribute("id");
+          visitorNumber = visitorNumber.slice(
+            visitorNumber.lastIndexOf("_") + 1
+          );
+
+          if (Number(visitorNumber) <= imageList.length - 1) continue;
+
+          // 미용실의 리뷰가 threshold를 초과한 경우
+          if (imageList.length >= MAX_IMAGE_LENGTH_PER_SALON) break;
+
+          let src = await imageElement.getAttribute("src");
+
+          imageList.push(src);
+        }
+
+        if (imageList.length >= MAX_IMAGE_LENGTH_PER_SALON) break;
+
+        // // 최초 30개씩 보여주므로 30개 아래면 스크롤할 필요 없음
+        if (reviewImages.length < 30) break;
+
+        let curHeight = prevHeight;
+
+        // 더 스크롤 가능한지 검사
+        await driver
+          .wait(async () => {
+            await driver.executeScript(script.scrollBottom);
+            await driver.sleep(SCROLL_TIME);
+
+            curHeight = await driver.executeScript(script.getScrollY);
+
+            return prevHeight !== curHeight;
+          }, SCROLL_TIMEOUT)
+          .catch(() => {});
+
+        // 무한스크롤이 끝난 경우
+        if (prevHeight === curHeight) break;
+
+        // 아닐경우 현재 height 갱신
+        prevHeight = curHeight;
+      }
+
+      console.log(`${placeName}: ${imageList.length}개`);
+
+      totalImageList = [...totalImageList, ...imageList];
+
+      await driver.navigate().back();
+    }
+
+    console.log(`\ntotal: ${totalImageList.length}개`);
+  } catch (e) {
+    console.log(e);
+  } finally {
+    if (!TEST_MODE) driver.quit();
+  }
+};
+
+console.time("running time");
+run().then(() => {
+  console.timeEnd("running time");
+});
